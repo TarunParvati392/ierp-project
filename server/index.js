fc20d7a6ef72d1d7e0afb9fc88f441ad81ebdc34
TarunParvati392 <yadavtarun392@gmail.com>
2025-07-30 00:35:51 +0530

    Cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://ierp-project-e42u.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect to MongoDB
